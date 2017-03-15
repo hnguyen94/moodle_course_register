58d48ade1e21ce28bbeacb645194724cd7b76f1b
Hoang Nguyen <hoang.nguyen@xing.com>
2017-03-15 17:25:19 +0100

    [WIP] Add it5l class

---
+++ it5l.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+class IT5L
+  def self.courses
+    @course = {
+      ae:     'http://moodle.itech-bs14.de/course/index.php?categoryid=5',
+      it_sys: 'https://moodle.itech-bs14.de/course/index.php?categoryid=6'
+    }
+  end
+end
